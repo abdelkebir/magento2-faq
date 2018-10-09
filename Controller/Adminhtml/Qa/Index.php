9857554ea7b98ef75dbae6c0f70a73012cd3add0
EL Harfali <elharfali.abdelkebir@gmail.com>
2018-10-09 17:07:58 +0100

    Admin Grids

---
+++ Controller/Adminhtml/Qa/Index.php
@@ -0,0 +1,20 @@
+<?php
+namespace Godogi\Faq\Controller\Adminhtml\Qa;
+class Index extends \Magento\Backend\App\Action
+{
+	protected $resultPageFactory = false;
+	public function __construct(
+		\Magento\Backend\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $resultPageFactory
+	)
+	{
+		parent::__construct($context);
+		$this->resultPageFactory = $resultPageFactory;
+	}
+	public function execute()
+	{
+		$resultPage = $this->resultPageFactory->create();
+		$resultPage->getConfig()->getTitle()->prepend((__('Questions / Answers')));
+		return $resultPage;
+	}
+}
