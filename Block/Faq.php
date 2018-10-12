a7480b1e18f6997aa6ad7d78c3adf933351feca1
EL Harfali <elharfali.abdelkebir@gmail.com>
2018-10-12 15:06:27 +0100

    Frontend Changes

--- Block/Faq.php
+++ Block/Faq.php
@@ -1,9 +1,28 @@
 <?php
 namespace Godogi\Faq\Block;
+
+use Magento\Framework\View\Element\Template\Context;
+use Godogi\Faq\Helper\Data as FaqData;
+
 class Faq extends \Magento\Framework\View\Element\Template
 {
-    public function getTxt()
+	protected $_faqHelper;
+	
+	public function __construct(
+				Context $context,
+				FaqData $faqHelper,
+				array $data = [])
+	{
+		$this->_faqHelper = $faqHelper;
+		parent::__construct($context, $data);
+	}
+    
+    public function getTopics()
     {
-        return 'FAQ';
+        return $this->_faqHelper->getTopics();
+    }
+    public function getQasByTopicId($topicId)
+    {
+        return $this->_faqHelper->getQasByTopicId($topicId);
     }
 }
